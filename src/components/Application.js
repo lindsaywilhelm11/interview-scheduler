2bbb1c18ee9741735abf1b9c047c8d847e5a1534
Lindsay Wilhelm <lindsaywilhelm@Lindsays.local>
2022-01-22 20:52:54 -0500

    adds custom hooks to appointment/index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,7 @@ import "components/Application.scss";
 import axios from "axios";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
+
 export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
